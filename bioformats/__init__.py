e2e5e93a7c160ec46847dfecc987ce30ccb2be11
Vebjorn Ljosa <vebjorn@ljosa.com>
2014-03-11 15:42:41 -0400

    Get version number from git

--- bioformats/__init__.py
+++ bioformats/__init__.py
@@ -8,7 +8,12 @@
 '''Bioformats package - wrapper for loci.bioformats java code
 
 '''
-__version__ = "$Revision$"
+
+try:
+    from _version import __version__
+except ImportError:
+    # We're running in a tree that doesn't have a _version.py, so we don't know what our version is.
+    __version__ = "0.0.0"
 
 import os.path
 import javabridge
