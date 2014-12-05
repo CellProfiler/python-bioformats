b157aaeca3f266b3bcf5b80ce746d532a58f7c0e
Lee Kamentsky <leek@broadinstitute.org>
2014-12-05 16:01:59 -0500

    The demo takes an optional image name on the command line and displays the image using pylab if it's installed

--- demo/demo.py
+++ demo/demo.py
@@ -4,15 +4,26 @@ import os
 import javabridge
 import bioformats
 from bioformats import log4j
+import sys
 
 javabridge.start_vm(class_path=bioformats.JARS,
                     run_headless=True)
 try:
     log4j.basic_config()
-    image_path = os.path.join(os.path.dirname(bioformats.__file__), 'tests', 
-                              'Channel1-01-A-01.tif')
+    if len(sys.argv) < 2:
+        image_path = os.path.join(os.path.dirname(bioformats.__file__), 'tests', 
+                                  'Channel1-01-A-01.tif')
+    else:
+        image_path = sys.argv[1]
     image, scale = bioformats.load_image(image_path, rescale=False,
                                          wants_max_intensity=True)
-    print image.shape
+    try:
+        import pylab
+        
+        pylab.imshow(image)
+        pylab.gca().set_title(image_path)
+        pylab.show()
+    except:
+        print image.shape
 finally:
     javabridge.kill_vm()
