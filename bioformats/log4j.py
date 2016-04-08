b068d99decf63d7965ad25823e04e9e43f274fad
Lee Kamentsky <leek@broadinstitute.org>
2016-04-08 09:18:37 -0400

    Fixes #53 rework basic_config so it uses loci.common.Log4jTools

---
+++ bioformats/log4j.py
@@ -0,0 +1,12 @@
+# Python-bioformats is distributed under the GNU General Public
+# License, but this file is licensed under the more permissive BSD
+# license.  See the accompanying file LICENSE for details.
+# 
+# Copyright (c) 2009-2014 Broad Institute
+# All rights reserved.
+
+import javabridge
+
+def basic_config():
+    '''Configure logging for "WARN" level'''
+    javabridge.JClassWrapper("loci.common.Log4jTools").enableLogging("WARN")
