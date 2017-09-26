9f5a41f89676e3f4a47e17b9ca1a9733e1f59d14
mcquin <mcquin@broadinstitute.org>
2017-09-26 13:21:38 -0400

    Fix demo script.

--- bioformats/log4j.py
+++ bioformats/log4j.py
@@ -1,7 +1,7 @@
 # Python-bioformats is distributed under the GNU General Public
 # License, but this file is licensed under the more permissive BSD
 # license.  See the accompanying file LICENSE for details.
-# 
+#
 # Copyright (c) 2009-2014 Broad Institute
 # All rights reserved.
 
@@ -9,4 +9,6 @@ import javabridge
 
 def basic_config():
     '''Configure logging for "WARN" level'''
-    javabridge.JClassWrapper("loci.common.Log4jTools").enableLogging("WARN")
+    log4j = javabridge.JClassWrapper("loci.common.Log4jTools")
+    log4j.enableLogging()
+    log4j.setRootLevel("WARN")
