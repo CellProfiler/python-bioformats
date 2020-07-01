ef4f11890a5685c42fc2ef7aa3f1c816eb7816f1
Allen Goodman <allen.goodman@icloud.com>
2020-07-01 14:11:43 -0400

    updated python-javabridge

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     install_requires=[
         "boto3",
         "future",
-        "python-javabridge"
+        "python-javabridge>=4.0.0rc2"
     ],
     license="GPL License",
     long_description="""Python-bioformats is a Python wrapper for Bio-Formats, a standalone Java library for reading
