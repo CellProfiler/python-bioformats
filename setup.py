9eca09c607fb2beab99d89cd4ccb94319f922848
Beth Cimini <beth.cimini@gmail.com>
2021-02-16 10:36:30 -0500

    v4.0.4

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     install_requires=[
         "boto3>=1.14.23",
         "future>=0.18.2",
-        "python-javabridge==4.0.2"
+        "python-javabridge==4.0.3"
     ],
     license="GPL License",
     long_description="""Python-bioformats is a Python wrapper for Bio-Formats, a standalone Java library for reading
@@ -38,6 +38,5 @@ setuptools.setup(
     packages=[
         "bioformats"
     ],
-    url="http://github.com/CellProfiler/python-bioformats/",
-    version="4.0.3"
+    url="http://github.com/CellProfiler/python-bioformats/"
 )
